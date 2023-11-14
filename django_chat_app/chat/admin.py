202ea2c10d4f4613bc371b4c8eaf7622dbd812ea
Kay <info@beckmann-md.de>
2023-11-14 21:16:19 +0100

    add chat model

--- django_chat_app/chat/admin.py
+++ django_chat_app/chat/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from chat.models import Message
+
+class MessageAdmin(admin.ModelAdmin):
+  fields = ('text','created_at', 'author', 'receiver') #detail view
+  search_fields = ('text',)
 
 # Register your models here.
+admin.site.register(Message)
--- django_chat_app/chat/models.py
+++ django_chat_app/chat/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.db.models.fields import DateField
+from datetime import date
+from django.conf import settings
 
 # Create your models here.
+class Message(models.Model):
+  text = models.CharField(max_length=512)
+  created_at = DateField(default=date.today)
+  # chat =chat klasse verknüpfen
+  author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='author_message_set')
+  receiver = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='receiver_message_set')
+
+  def __str__(self): #Overview in adminpanel
+    return f"{self.created_at}: {self.author} -> {self.text} "
