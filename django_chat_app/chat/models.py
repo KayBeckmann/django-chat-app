0c002fcdb3d7eb55d955a577a763492edcc46669
Kay <info@beckmann-md.de>
2023-11-19 11:10:21 +0100

    save message to DB

--- django_chat_app/chat/models.py
+++ django_chat_app/chat/models.py
@@ -4,19 +4,21 @@ from datetime import date
 from django.conf import settings
 
 # Create your models here.
-class Message(models.Model):
-  text = models.CharField(max_length=512)
-  created_at = DateField(default=date.today)
-  # chat =chat klasse verknüpfen
-  author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='author_message_set')
-  receiver = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='receiver_message_set')
-
-  def __str__(self): #Overview in adminpanel
-    return f"{self.created_at}: {self.author} -> {self.text} "
-
 
 class Chat(models.Model):
   created_at = DateField(default=date.today)
 
   def __str__(self): #Overview in adminpanel
     return f"{self.created_at}"
+class Message(models.Model):
+  text = models.CharField(max_length=512)
+  created_at = DateField(default=date.today)
+  # chat =chat klasse verknüpfen
+  author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='author_message_set')
+  receiver = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='receiver_message_set')
+  chat = models.ForeignKey(Chat, on_delete=models.CASCADE, related_name='chat_message_set', default=None, blank=True, null=True)
+
+  def __str__(self): #Overview in adminpanel
+    return f"{self.created_at}: {self.author} -> {self.text} "
+
+
--- django_chat_app/chat/views.py
+++ django_chat_app/chat/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
+from .models import Message
+from .models import Chat
 
 # Create your views here.
 def index(request):
   if request.method == 'POST':
+    myChat = Chat.objects.get(id=1)
     print("Received data: " + request.POST['message'])
+    Message.objects.create(text=request.POST['message'], author=request.user, receiver=request.user, chat=myChat)
   return render(request, 'chat/index.html', {'username':'Kay'})
